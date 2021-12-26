4025acf57d6c8fe458b740fdf8b04af3cbe3d191
Chronos386 <popersoniy@gmail.com>
2021-12-26 17:21:38 +0300

    Добавлена панель для просмотра и изменения базы данных администратором (пока что без функционала)

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from all_models import *
+from adm_db_panel import *
+import sys
 
 for i in session.query(Character).all():
     print(i)
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
