f9aa703786061d73a58ca38ac8946c716e34dfe3
Chronos386 <popersoniy@gmail.com>
2022-01-01 03:47:21 +0300

    Откомментировал main обратно

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-'''
 from interface_classes import *
-import sys
-from backup_db import *
-'''
-'''
+
 app = QApplication(sys.argv)
 window = log_panel()
 window.show()
 sys.exit(app.exec_())
-'''
-'''
-# dump PostgreSQL
-postgresqlResults = DumpPostgreSql("postgres")
-print(postgresqlResults)
-'''
-
-from backup_db import *
-
-DumpPostgreSql()
